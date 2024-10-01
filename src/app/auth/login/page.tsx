2b1fef24354a0358917884176a533f69e99238d3
ujjwal_singh_basnet <asujjwalbasnyat@gmail.com>
2024-10-01 20:04:35 +0545

    integrate custom css

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,19 @@
 import LoginForm from "./components/loginForm";
+import { FcGoogle } from "react-icons/fc";
 
 export default function Login() {
 	return (
 		<div>
 			Welcome Login!.
 			<LoginForm />
+			<p>or</p>
+			<div>
+				<FcGoogle />
+				<a href="#">Login with Google</a>
+			</div>
+			<p>
+				Are you new? <a href="#">Create an Account</a>
+			</p>
 		</div>
 	);
 }
